6ce32a9765438965f6ab53e3744f470733d4d995
Adam <adamb56789@gmail.com>
2020-11-09 19:45:30 +0000

    Added ConfinementArea

---
+++ src/main/java/uk/ac/ed/inf/aqmaps/ConfinementArea.java
@@ -0,0 +1,15 @@
+package uk.ac.ed.inf.aqmaps;
+
+/**
+ * Holds information about the confinement area and checking whether a point is inside it.
+ */
+public class ConfinementArea {
+    /** A Point representing the northwest corner of the confinement area. */
+    private static final Coords TOP_LEFT = new Coords(-3.192473, 55.946233);
+    /** A Point representing the southeast corner of the confinement area. */
+    private static final Coords BOTTOM_RIGHT = new Coords(-3.184319, 55.942617);
+
+    public static boolean isInConfinement(Coords point) {
+        return false; //TODO
+    }
+}
